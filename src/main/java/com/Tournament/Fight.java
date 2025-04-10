d38c1f499bda0a21b190bf6f12df92e6ee89d240
Jayde Howell <jayde.m.howell@gmail.com>
2025-04-10 07:22:45 -0700

    Adding the beginnings of fight logic

--- src/main/java/com/Tournament/Fight.java
+++ src/main/java/com/Tournament/Fight.java
@@ -1,7 +1,13 @@
 package com.Tournament;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fight {
     private int round = 1;
+    private Dice fightDice = new Dice(20, "Fight");
+    private ArrayList<Integer> firstJudgeScore1 = new ArrayList<>();
+    private ArrayList<Integer> firstJudgeScore2 = new ArrayList<>();
 
     public void fightNight(Fighter fighter1, Fighter fighter2) {
         if (round == 1) {
@@ -18,13 +24,31 @@ public class Fight {
     public void nextRound(Fighter fighter1, Fighter fighter2, Integer round) {
         Console.printLargePause("Round #" + round);
 
-        attack(fighter1, fighter2);
+        int fighter1Exchange = fightDice.rollDice(1);
+        int fighter2Exchange = fightDice.rollDice(1);
 
-        if (fighter2.isAlive()) {
-            Console.printSmallPause(fighter2.getName() + " has "
-                    + fighter2.getCurrentHealth() + " health remaining");
+        if (fighter1Exchange > fighter2Exchange) {
+            Console.printSmallPause("Looks like " + fighter1
+                    + " is getting the better of " + fighter2);
+            firstJudgeScore1.add(10);
+            firstJudgeScore2.add(9);
         } else {
-            Console.printLargePause("It's over!");
+            Console.printSmallPause("Looks like " + fighter2
+                    + " is getting the better of " + fighter1);
+            firstJudgeScore1.add(9);
+            firstJudgeScore2.add(10);
         }
+    }
+
+    public int getRound() {
+        return round;
+    }
+
+    public ArrayList<Integer> getFirstJudgeScore1() {
+        return firstJudgeScore1;
+    }
+
+    public ArrayList<Integer> getFirstJudgeScore2() {
+        return firstJudgeScore2;
     }
 }
