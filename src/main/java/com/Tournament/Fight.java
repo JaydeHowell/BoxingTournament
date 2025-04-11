92c5ea597386dc6ef349a1612c0636390b30f879
Jayde Howell <jayde.m.howell@gmail.com>
2025-04-11 07:39:01 -0700

    added exchange logic and some debug text

--- src/main/java/com/Tournament/Fight.java
+++ src/main/java/com/Tournament/Fight.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 public class Fight {
     private int round = 1;
-    private Dice fightDice = new Dice(20, "Fight", false);
+    private Dice hitDice = new Dice(40, "Hit", false);
+    private final int EXCHANGE_THRESHOLD = 18;
     //score 1 = fighter 1; score 2 = fighter 2
     private ArrayList<Integer> firstJudgeScore1 = new ArrayList<>();
     private ArrayList<Integer> firstJudgeScore2 = new ArrayList<>();
@@ -51,20 +52,57 @@ public class Fight {
     }
     public void nextRound(Fighter fighter1, Fighter fighter2, Integer round) {
         Console.printLargePause("Round #" + round);
+        int fighter1Performance = 1;
+        int fighter2Performance = 1;
 
-        int fighter1Exchange = fightDice.rollDice(1);
-        int fighter2Exchange = fightDice.rollDice(1);
+        for(int i = 0; i <= 3; i++) {
+            if (willExchange(fighter1, fighter2)) {
+                if (exchange(fighter1, fighter2) == fighter1) {
+                    fighter1Performance++;
+                } else {
+                    fighter2Performance++;
+                }
+            } else {
+                Console.printLargePause("They're feeling each other out, but no action.");
+            }
+        }
+        fighter1Performance = hitDice.rollDice(1) * fighter1Performance;
+        fighter2Performance = hitDice.rollDice(1) * fighter2Performance;
 
-        if (fighter1Exchange > fighter2Exchange) {
-            Console.printSmallPause("Looks like " + fighter1.getName()
-                    + " is getting the better of " + fighter2.getName());
+        if (fighter1Performance > fighter2Performance) {
             firstJudgeScore1.add(10);
             firstJudgeScore2.add(9);
         } else {
-            Console.printSmallPause("Looks like " + fighter2.getName()
-                    + " is getting the better of " + fighter1.getName());
             firstJudgeScore1.add(9);
             firstJudgeScore2.add(10);
+        }
+        Console.printLargePause(fighter1Performance
+                + " to " + fighter2Performance);
+    }
+
+    public boolean willExchange(Fighter fighter1, Fighter fighter2) {
+        int exchangeModifier = fighter1.getDexterity() + fighter2.getDexterity();
+        int exchangeResult = hitDice.rollDice(1) + exchangeModifier;
+        if (fighter1 instanceof Slugger || fighter2 instanceof Slugger) {
+            exchangeResult = exchangeResult * 2;
+        }
+        if (fighter1 instanceof Slick || fighter2 instanceof Slick) {
+            exchangeResult = exchangeResult / 2;
+        }
+        return EXCHANGE_THRESHOLD <= exchangeResult;
+    }
+
+    public Fighter exchange(Fighter fighter1, Fighter fighter2) {
+        int exchangeWinner = (int) (Math.random() * 100);
+        Console.printLargePause("What a lovely exchange!");
+        if (exchangeWinner <= 50) {
+            Console.printSmallPause("Looks like " + fighter1.getName()
+                    + " is getting the better of " + fighter2.getName() + " (" + exchangeWinner);
+            return fighter1;
+        } else {
+            Console.printSmallPause("Looks like " + fighter2.getName()
+                    + " is getting the better of " + fighter1.getName() + " (" + exchangeWinner);
+            return fighter2;
         }
     }
 
