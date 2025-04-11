f2b0bea3943d2d0a4e7fa037d8ed3500f8bfad76
Jayde Howell <jayde.m.howell@gmail.com>
2025-04-11 07:38:46 -0700

    lowered stats into multipliers for algorithms

--- src/main/java/com/Tournament/Main.java
+++ src/main/java/com/Tournament/Main.java
@@ -2,8 +2,8 @@ package com.Tournament;
 
 public class Main {
     public static void main(String[] args) {
-        Slick firstFighter = new Slick("Sweet", 12, 16, 14, 16);
-        Slugger secondFighter = new Slugger("Scruff", 16, 14, 16, 12);
+        Slick firstFighter = new Slick("Sweet", 1, 3, 2, 3);
+        Slugger secondFighter = new Slugger("Scruff", 3, 2, 3, 1);
         Fight firstFight = new Fight();
         firstFight.fightNight(firstFighter, secondFighter);
     }
