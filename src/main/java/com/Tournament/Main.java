c93624b35d5cc4fed4e835657f7f1eaae0eccc0b
Jayde Howell <jayde.m.howell@gmail.com>
2025-04-11 05:34:28 -0700

    added main logic to actually initiate a fight

--- src/main/java/com/Tournament/Main.java
+++ src/main/java/com/Tournament/Main.java
@@ -2,6 +2,9 @@ package com.Tournament;
 
 public class Main {
     public static void main(String[] args) {
-
+        Slick firstFighter = new Slick("Sweet", 12, 16, 14, 16);
+        Slugger secondFighter = new Slugger("Scruff", 16, 14, 16, 12);
+        Fight firstFight = new Fight();
+        firstFight.fightNight(firstFighter, secondFighter);
     }
 }
