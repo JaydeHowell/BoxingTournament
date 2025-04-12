fba2c42b68172d1a4daaab82383e009204b2185c
Jayde Howell <jayde.m.howell@gmail.com>
2025-04-12 06:50:30 -0700

    added text reading logic and validation for fighter creation

--- src/main/java/com/Tournament/Console.java
+++ src/main/java/com/Tournament/Console.java
@@ -5,6 +5,44 @@ import java.util.Scanner;
 public class Console {
     private static Scanner scanner = new Scanner(System.in);
 
+    public static String readChoice(String prompt, String[] options) {
+        int choiceNumber = -1;
+
+        while (true) {
+            System.out.println(prompt);
+            for (int i = 0; i < options.length; i++) {
+                System.out.println((i + 1) + ") " + options[i]);
+            }
+            System.out.println("Enter your choice number: ");
+            //checking if input is an integer and within the bounds of choices
+            if (scanner.hasNextInt()) {
+                choiceNumber = scanner.nextInt();
+                if (choiceNumber >=1 && choiceNumber <= options.length) {
+                    break;
+                }
+            } else {
+                // clears input
+                scanner.next();
+            }
+            System.out.println("Invalid. Please select a number from the available options.");
+        }
+        return options[choiceNumber -1];
+    }
+
+    public static String readText(String prompt) {
+        String value;
+        while (true) {
+            System.out.println(prompt);
+            value = scanner.nextLine().trim();
+            if (value.isEmpty()) {
+                System.out.println("Please type a name for your character.");
+            } else {
+                break;
+            }
+        }
+        return value;
+    }
+
     public static void pause(int milliseconds) {
         try {
             Thread.sleep(milliseconds);
