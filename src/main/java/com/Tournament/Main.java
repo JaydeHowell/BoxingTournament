064431a519eceb565624c9f031b54fb84c3b1129
Jayde Howell <jayde.m.howell@gmail.com>
2025-04-12 19:28:41 -0700

    completing tournament logic passing arrays

--- src/main/java/com/Tournament/Main.java
+++ src/main/java/com/Tournament/Main.java
@@ -2,7 +2,7 @@ package com.Tournament;
 
 public class Main {
     public static void main(String[] args) {
-        Tournament newTournament = new Tournament(4);
+        Tournament newTournament = new Tournament(8);
         newTournament.start();
     }
 }
--- src/main/java/com/Tournament/Tournament.java
+++ src/main/java/com/Tournament/Tournament.java
@@ -22,12 +22,42 @@ public class Tournament {
     }
 
     public void start() {
-        List<Fighter> bracket = setup();
+        List<Fighter> bracketStart = setup();
         Console.printLargePause("Here are the participants");
         for (int i = 0; i < participants; i++) {
-            Console.printSmallPause(i+1 + ": " + bracket.get(i).getName());
+            Console.printSmallPause(i+1 + ": " + bracketStart.get(i).getName());
         }
         Console.printLargePause("Let the tournament begin!");
+        Fighter winner = tournamentFunctions(bracketStart);
+        Console.printLargePause("After a grueling tournament, your winner is...");
+        Console.printLargePause(winner.getName() + "!!!");
+    }
+
+    private Fighter tournamentFunctions(List<Fighter> fighters) {
+        int tournamentRounds = ExponentSolver.solve(2, participants);
+        List<Fighter> nextRound = fighters;
+        for (int j = 0; j < tournamentRounds; j++) {
+            nextRound = roundOne(nextRound);
+            if (nextRound.size() > 1) {
+                Console.printLargePause("Here are the remaining participants");
+                for (int i = 0; i < nextRound.size(); i++) {
+                    Console.printSmallPause(i + 1 + ": " + nextRound.get(i).getName());
+                }
+            }
+        }
+        return nextRound.getFirst();
+    }
+
+    private List<Fighter> roundOne(List<Fighter> fighters) {
+        List<Fighter> newBracket = new ArrayList<>();
+        for (int i = 0; i < fighters.size()/2; i++) {
+            //get the seed
+            Fighter winner = new Fight().fightNight(fighters.get(i),
+                    //get the reverse seed
+                    fighters.get(fighters.size()-(i+1)));
+            newBracket.add(winner);
+        }
+        return newBracket;
     }
 
     public int getParticipants() {
